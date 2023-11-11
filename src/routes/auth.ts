7a27d9718eefc4fed0b60bfe3afe31e353a4c842
Vitalijs Pankovs <vitalijs.pankovs@gmail.com>
2023-11-11 22:15:18 +0200

    ne poluchilosj

---
+++ src/routes/auth.ts
@@ -0,0 +1,19 @@
+// import * from "react";
+// import { RouteObject } from "react-router-dom";
+// import AuthPage from '../pages/auth/AuthPage.tsx';
+// import LoginForm from "../pages/auth/components/LoginForm";
+
+// const route: RouteObject = {
+//   path: 'auth',
+//   element: AuthPage,
+//   children: [
+//     {
+//       path: 'login',
+//       element: LoginForm,
+//     },
+//     {
+//       path: 'register',
+//       element: RegisterForm,
+//     }
+//   ]
+// }
