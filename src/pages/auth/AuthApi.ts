cebf216cf1067af4ce8739d26c4c6f450fabcae7
Vitalijs Pankovs <vitalijs.pankovs@gmail.com>
2023-11-28 22:52:33 +0200

    Fixed AuthApi.

--- src/pages/auth/AuthApi.ts
+++ src/pages/auth/AuthApi.ts
@@ -3,10 +3,33 @@ import { SessionDTO } from "@/store/session/types"
 
 export default abstract class AuthApi {
   static async login(email: string, password: string) {
-    return await http.post<SessionDTO>('auth/login', { email, password })
+    try {
+      const dto = await http.post<SessionDTO>('auth/login', { email, password })
+      return AuthApi.handleSuccess(dto)
+    } catch (error) {
+      AuthApi.handleError(error)
+    }
   }
 
   static async register(email: string, username: string, password: string) {
-    return await http.post<SessionDTO>('auth/register', { email, username, password })
+    try {
+      const dto = await http.post<SessionDTO>('auth/register', { email, username, password })
+      return AuthApi.handleSuccess(dto)
+    } catch (error) {
+      AuthApi.handleError(error)
+    }
+  }
+
+  private static handleSuccess(dto: SessionDTO): SessionDTO {
+    return {
+      expiresAt: Number(dto.expiresAt),
+      token: String(dto.token)
+    }
+  }
+
+  private static handleError(error: any): never {
+    // TODO: Exceptions
+
+    throw error
   }
 }
