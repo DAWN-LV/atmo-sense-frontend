ae8772f8280572f7b7395bb1d4b2df80de21bb85
Vitalijs Pankovs <vitalijs.pankovs@gmail.com>
2023-12-06 14:06:01 +0200

    Changed dialog button position.

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,14 +1,20 @@
 import Breadcrumb, { Props as BreadcrumbProps } from "@/components/Breadcrumb"
 
 type Props = BreadcrumbProps & {
-  children: React.ReactNode
+  children: React.ReactNode,
+  prepend?: React.ReactNode
 }
 
-const Page: React.FC<Props> = ({ children, breadcrumb }) => (
+const Page: React.FC<Props> = ({ children, prepend, breadcrumb }) => (
   <>
     <nav className="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start" id="navbarTop" navbar-scroll="true">
       <div className="flex items-center justify-between w-full px-1 py-1 mx-auto flex-wrap-inherit">
         <Breadcrumb breadcrumb={ breadcrumb }/>
+        { prepend ? (
+          <div className="flex items-center">
+            { prepend }
+          </div>
+        ) : null }
       </div>
     </nav>
     <main className="w-full px-6 py-4 mx-auto">
